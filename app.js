0231d829393456040f655dced03ab0d2a4a68a0e
Yasin Yavuz <barbaros1675@gmail.com>
2022-08-15 09:03:38 +0200

    JS conditionals

---
+++ app.js
@@ -0,0 +1,8 @@
+const number = prompt("Enter a number: ")
+
+if (number > 0){
+    console.log("The number is positive");
+} else {
+console.log("The number is either a negative number or 0");
+}
+console.log("The if statement is easy");
