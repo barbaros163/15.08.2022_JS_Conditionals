1142f0bd1bbdda956ac69a89e18a65baf39acd24
Yasin Yavuz <barbaros1675@gmail.com>
2022-08-15 09:50:32 +0200

    js conditionals

--- app.js
+++ app.js
@@ -1,8 +1,92 @@
-const number = prompt("Enter a number: ")
+// const number = prompt("Enter a number: ");
+// // enter either positive or negative number to check the output
+// // check if number is greater than 0
+// if (number > 0) {
+//   // the body of the if statement
+//   console.log("The number is positive");
+// }
+// console.log("The if statement is easy");
 
-if (number > 0){
-    console.log("The number is positive");
-} else {
-console.log("The number is either a negative number or 0");
+// const number = prompt("Enter a number: ");
+// // check if number is greater than 0
+// if (number > 0) {
+//   console.log("The number is positive");
+// } else { // if number is not greater than 0
+//   console.log("The number is either a negative number or 0");
+// }
+// console.log("The if...else statement is easy");
+
+// const number = prompt("Enter a number: ");
+// // check if number is greater than 0
+// if (number > 0) {
+//   console.log("The number is positive");
+// } else if (number == 0) { // check if number is 0
+//   console.log("The number is 0");
+// } else { // if number is neither greater than 0, nor zero
+//   console.log("The number is negative");
+// }
+// console.log("The if...else if...else statement is easy");
+
+// const age = 15;
+// let result =
+//     (age >= 18) ? "You are legal." : "You are not legal yet.";
+// console.log(result);
+
+// let a = 3;
+// let result = (a >= 0) ? (a == 0 ? "zero" : "positive") : "negative";
+// console.log(`The number is ${result}.`)
+// const first = 0;
+// const second = 20;
+// const third = 30;
+// let result = first && second && third;
+// console.log(result);
+
+// const userLoggedIn = true;
+// userLoggedIn && console.log('Welcome'); 
+
+// let day = 2;
+// let dayName;
+// switch (day) {
+//   case 1:
+//     dayName = 'Monday';
+//     break;
+//   case 2:
+//     dayName = 'Tuesday';
+//     break;
+//   case 3:
+//     dayName = 'Wednesday';
+//     break;
+//   case 4:
+//     dayName = 'Thursday';
+//     break;
+//   case 5:
+//     dayName = 'Friday';
+//     break;
+//   case 6:
+//     dayName = 'Saturday';
+//     break;
+//   case 7:
+//     dayName = 'Sunday';
+//     break;
+//   default:
+//     dayName = 'Invalid day';
+// }
+// console.log(dayName); // Tuesday
+
+let a = 7;
+
+switch (a) {
+
+ case 1:
+  console.log(a);
+  break;
+ case 2:
+  console.log(a+2);
+  break;
+  case 3:
+  console.log(a+4);
+  break;
+ default:
+  console.log(a+3);
+
 }
-console.log("The if statement is easy");
