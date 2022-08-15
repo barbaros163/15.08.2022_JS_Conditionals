381f367f4e975cb8e2ef02b24ac5dc456bd00a1d
Yasin Yavuz <barbaros1675@gmail.com>
2022-08-15 22:31:41 +0200

    conditions

--- app.js
+++ app.js
@@ -1,92 +1,88 @@
-// const number = prompt("Enter a number: ");
-// // enter either positive or negative number to check the output
-// // check if number is greater than 0
-// if (number > 0) {
-//   // the body of the if statement
-//   console.log("The number is positive");
-// }
-// console.log("The if statement is easy");
+// * ===============================================
+// *                  KARAR YAPILARI
+// * ===============================================
 
-// const number = prompt("Enter a number: ");
-// // check if number is greater than 0
-// if (number > 0) {
-//   console.log("The number is positive");
-// } else { // if number is not greater than 0
-//   console.log("The number is either a negative number or 0");
-// }
-// console.log("The if...else statement is easy");
+//**************** IF-ELSE *******************
 
-// const number = prompt("Enter a number: ");
-// // check if number is greater than 0
-// if (number > 0) {
-//   console.log("The number is positive");
-// } else if (number == 0) { // check if number is 0
-//   console.log("The number is 0");
-// } else { // if number is neither greater than 0, nor zero
-//   console.log("The number is negative");
-// }
-// console.log("The if...else if...else statement is easy");
+console.log("****** CONDITIONS *******");
 
-// const age = 15;
-// let result =
-//     (age >= 18) ? "You are legal." : "You are not legal yet.";
-// console.log(result);
+//? ORNEK1: Console'dan girilen bir sayisinin pozitif,negatif
+//? yada 0 oldugunu tespit ederek yazdiriniz.
 
-// let a = 3;
-// let result = (a >= 0) ? (a == 0 ? "zero" : "positive") : "negative";
-// console.log(`The number is ${result}.`)
-// const first = 0;
-// const second = 20;
-// const third = 30;
-// let result = first && second && third;
-// console.log(result);
-
-// const userLoggedIn = true;
-// userLoggedIn && console.log('Welcome'); 
-
-// let day = 2;
-// let dayName;
-// switch (day) {
-//   case 1:
-//     dayName = 'Monday';
-//     break;
-//   case 2:
-//     dayName = 'Tuesday';
-//     break;
-//   case 3:
-//     dayName = 'Wednesday';
-//     break;
-//   case 4:
-//     dayName = 'Thursday';
-//     break;
-//   case 5:
-//     dayName = 'Friday';
-//     break;
-//   case 6:
-//     dayName = 'Saturday';
-//     break;
-//   case 7:
-//     dayName = 'Sunday';
-//     break;
-//   default:
-//     dayName = 'Invalid day';
-// }
-// console.log(dayName); // Tuesday
-
-let a = 7;
-
-switch (a) {
-
- case 1:
-  console.log(a);
-  break;
- case 2:
-  console.log(a+2);
-  break;
-  case 3:
-  console.log(a+4);
-  break;
- default:
-  console.log(a+3);
-
+const sayi = Number(prompt("Bir sayi giriniz:"));
+if (sayi > 0) {
+  console.log(`${sayi} pozitiftir.`);
+} else if (sayi == 0) {
+  console.log(`${sayi}'a esittir.`);
+} else {
+  console.log(`${sayi} negatiftir.`);
 }
+console.log(sayi, typeof sayi);
+
+//? ORNEK2: console'dan 3 tamsayi alarak bunlarin en buyugunu
+//? yazdiriniz.
+const n1 = +prompt("Sayi1:");
+const n2 = +prompt("Sayi2:");
+const n3 = +prompt("Sayi3:");
+
+//* 1.Yontem
+if (n1 >= n2 && n1 >= n3) {
+  console.log(`${n1} en buyuk sayidir`);
+} else if (n2 >= n1 && n2 >= n3) {
+  console.log(`${n2} en buyuk sayidir`);
+} else if (n3 >= n1 && n3 >= n2) {
+  console.log(`${n3} en buyuk sayidir`);
+}
+
+//* 2.Yontem
+let enBuyuk = n1;
+
+if (n2 >= enBuyuk) {
+  enBuyuk = n2;
+}
+if (n3 >= enBuyuk) {
+  enBuyuk = n3;
+}
+console.log(`${enBuyuk} en buyuktur`);
+
+//! ODEV1: Dort Islem Hesap Makinasi
+
+//**************** SWITCH-CASE *******************
+
+//! ODEV2
+//! Clarusway’deki haftalık ders ve etkinlik programınızı, console’dan girilen gün değerine göre çıktı veren kodu switch-case yapısı ile yazınız.
+
+//! Program
+//! Pazartesi, Salı ,Çarşamba, Perşembe -> InClass
+//! Cuma -> Teamwork
+//! Cumartesi ->  InClass + Workshop
+//! Pazar -> Self-Study
+//! Aksi takdirde -> Yanlis gun girildi.
+
+//**************** TERNARY *******************
+
+//? ORNEK5: Kaldi-Gecti
+const not = +prompt("Notunuzu giriniz:");
+// let sonuc = not >= 50 ? "Gecti" : "Kaldi";
+// console.log(sonuc);
+console.log(not >= 50 ? "Gecti" : "Kaldi");
+
+//? ORNEK6:
+const yas = 17;
+const cinsiyet = "erkek";
+const saglikli = false;
+
+yas >= 18 && cinsiyet === "erkek" && saglikli
+  ? console.log("askerlik yapmali")
+  : console.log("Askerlik yapmasina gerek yok");
+
+//! ODEV3:Maasi asgari ucretten az olanlara %50 zam,
+//! fazla olanlara ise %10 zam yapmak istiyoruz.
+
+//* let maas = 5000;
+//* maas = maas * 1.5
+
+//! ODEV4: Console’dan kişinin gelir ve gider miktarını alan
+//! eğer kişinin geliri giderinden en az asgari ücret kadar fazla ise Kredi Verilebilir 🤑
+//! değilse  Kredi Verilemez 🥺
+//! şeklinde çıktı veren kodu Ternary deyimi kullanarak yazınız.
